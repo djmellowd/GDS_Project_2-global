d1271dff42bfc31a17627aa083cee0d56e2963a4
Pawel Milczanowski <pawel.milczanowski@outlook.com>
2021-02-19 00:51:18 +0100

    Pathfinding System Coding Part 1

---
+++ Assets/Scripts/GridCombatSystem.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GridCombatSystem : MonoBehaviour
+{
+    [SerializeField] private UnitGridCombat unitGridCombat;
+
+    private void Start()
+    {
+        Grid<GridObject> grid = GameHandler_GridCombatSystem.Instance.GetGrid();
+        GridPathfinding gridPathfinding = GameHandler_GridCombatSystem.Instance.gridPathfinding;
+
+        grid.GetXY(unitGridCombat.GetPosition(), out int unitX, out int unitY);
+
+        GameHandler_GridCombatSystem.Instance.GetMovementTilemap().SetAllTilemapSprite(MovementTilemap.TilemapObject.TilemapSprite.None);
+
+        for (int x = 0; x < grid.GetWidth(); x++)
+        {
+            for (int y = 0; y < grid.GetHeight(); y++)
+            {
+                grid.GetGridObject(x, y).SetIsValidMovePosition(false);
+            }
+        }
+
+        int maxMoveDistance = 5;
+        for (int x = unitX - maxMoveDistance; x <= unitY + maxMoveDistance; x++)
+        {
+            for (int y = unitY - maxMoveDistance; y <= unitY + maxMoveDistance; y++)
+            {
+                if (gridPathfinding.IsWalkable(x, y))
+                {
+                    if (gridPathfinding.HasPath(unitX, unitY, x, y))
+                    {
+                        if (gridPathfinding.GetPath(unitX, unitY, x, y).Count <= maxMoveDistance)
+                        {
+                            GameHandler_GridCombatSystem.Instance.GetMovementTilemap().SetTilemapSprite(x, y, MovementTilemap.TilemapObject.TilemapSprite.Move);
+
+                            grid.GetGridObject(x, y).SetIsValidMovePosition(true);
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (GameHandler_GridCombatSystem.Instance.GetGrid().GetGridObject(UtilsClass.GetMouseWorldPosition()).GetIsValidMovePosition())
+            {
+                unitGridCombat.MoveTo(UtilsClass.GetMouseWorldPosition(), () => { });
+            }
+        }
+    }
+
+    public class EmptyGridObject
+    {
+        private Grid<EmptyGridObject> grid;
+        private int x;
+        private int y;
+        private bool isValidMovePosition;
+
+        public EmptyGridObject(Grid<EmptyGridObject> grid, int x, int y)
+        {
+            this.grid = grid;
+            this.x = x;
+            this.y = y;
+        }
+
+        public void SetIsValidMovePosition(bool set)
+        {
+            isValidMovePosition = set;
+        }
+
+        public bool GetIsValidMovePosition()
+        {
+            return isValidMovePosition;
+        }
+    }
+}
---
+++ Assets/Scripts/UnitGridCombat.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class UnitGridCombat : MonoBehaviour
+{
+    [SerializeField] private Team team;
+    private Character_Base characterBase;
+    private HealthSystem healthSystem;
+    private GameObject selectedGameObject;
+    private MovePositionPathfinding movePosition;
+    private State state;
+    private World_Bar healthBar;
+    public enum Team { };
+    private enum State { };
+
+    private void Awake()
+    {
+
+    }
+
+    private void HealthSystem_OnHealthChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void SetSelectedVisible(bool visible)
+    {
+
+    }
+
+    public void MoveTo(Vector3 targetPosition, Action onReachedPosition)
+    {
+
+    }
+
+    public bool CanAttackUnit(UnitGridCombat unitGridCombat)
+    {
+        return Vector3.Distance(GetPosition(), unitGridCombat.GetPosition()) < 60f;
+    }
+
+    public void AttackUnit(UnitGridCombat unitGridCombat, Action onAttackComplete)
+    {
+
+    }
+
+    private void ShootUnit(UnitGridCombat unitGridCombat, Action onShotComplete)
+    {
+
+    }
+
+    public void Damage(UnitGridCombat attacker, int damageAmount)
+    {
+
+    }
+
+    public bool isDead()
+    {
+
+    }
+
+    public Vector3 GetPosition()
+    {
+
+    }
+}
